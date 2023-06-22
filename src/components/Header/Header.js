4ee4e216b929c99bd9b5d87b5608ac83217246df
jeremy koundour <j.koundour@gmail.com>
2023-06-22 16:13:47 +0200

    suppression de bootstrap pour mise en forme

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import '../../css/components/Header.css';
 import logoNba from '../../assets/logos/SVG-nba.svg';
-// import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import NavbarHeader from './Navbar';
 
 export default function Header() {
   return (
-    <div className="Header">
-      <Navbar>
-        <Container id="containHeader">
-          <Navbar.Brand href="/">
-            <img
-              alt="logoHeader"
-              src={logoNba}
-              width="100"
-              height="100"
-              className="d-inline-block align-top"
-            />{' '}
-            <p>Land</p>
-          </Navbar.Brand>
-          <NavbarHeader />
-        </Container>
-      </Navbar>
-    </div>
+    <header className="Header">
+      <div className="logoHeader">
+        <Link to={'/'}>
+          <img
+            alt="logoHeader"
+            src={logoNba}
+            width="100"
+            height="100"
+            className="d-inline-block align-top"
+          />{' '}
+          <p>Land</p>
+        </Link>
+      </div>
+      <NavbarHeader />
+    </header>
   );
 }
+
+/* 
+          
+
+*/
