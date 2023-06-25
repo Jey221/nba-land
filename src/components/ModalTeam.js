4dbfeb3944e9ef209d7326c38f2ba07b04face42
jeremy koundour <j.koundour@gmail.com>
2023-06-25 18:06:17 +0200

    WIP: mise en forme modal

--- src/components/ModalTeam.js
+++ src/components/ModalTeam.js
@@ -1,16 +1,45 @@
-/* import { useEffect, useState } from 'react';
-import { getDataTeams } from '../data/datas';
- */
+import '../css/components/ModalTeam.css';
+import * as AiIcons from 'react-icons/ai';
+import LOGOS from '../data/LogosTeams';
+
 export default function ModalTeam({ setIsOpenModal, selectedTeam }) {
   console.log('selectedTeam', selectedTeam);
   console.log('selectedTeam', selectedTeam.name);
+  const logo = LOGOS[selectedTeam.abbreviation];
 
   return (
-    <div className="modalBackground">
-      <div className="modalContent">
-        <div className="modalHeader"></div>
-        <div className="modalBody"></div>
+    <div className="modalBackground" onClick={setIsOpenModal.bind(this, false)}>
+      <div className="modalContent" onClick={(e) => e.stopPropagation()}>
+        <header className="modalHeader">
+          <h1>{selectedTeam.full_name}</h1>
+          <div className="closeZone" onClick={setIsOpenModal.bind(this, false)}>
+            <AiIcons.AiOutlineCloseCircle />
+          </div>
+        </header>
+        <div className="modalBody">
+          <div className="logoZone">
+            <img
+              src={logo}
+              alt={selectedTeam.abbreviation}
+              className="logoTeam"
+              id={selectedTeam.name}
+            />
+            <h2>{selectedTeam.name}</h2>
+          </div>
+          <div className="infos">
+            <ul>
+              <li>{selectedTeam.city}</li>
+              <li>{selectedTeam.conference}</li>
+              <li>{selectedTeam.division}</li>
+              <li>{selectedTeam.abbreviation}</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
+
+/* 
+<AiOutlineCloseCircle/>
+*/
